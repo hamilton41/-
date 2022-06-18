c25e54d1ec9aea13e166d8d91d5da984221cabc8
hamilton41 <hamilton032123796@gmail.com>
2022-06-18 20:33:15 +0800

    s

---
+++ shoptify/shoptify.js
@@ -0,0 +1,10 @@
+const hiddenbox1 = document.querySelector("#hiddenbox1");
+
+function hiddenbox(index) {
+    switch (index) {
+        case 1:
+            hiddenbox1.classList.toggle("visibility");
+            break;
+
+    }
+}
